5877777cd7d6d8ff87a90e5176ddf17cbe5052ee
KlowdyTMS <klowdyacrux@gmail.com>
2023-11-07 17:50:42 -0300

    Initial commit

---
+++ Services/CustomerService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProvaPub.Models;
+using ProvaPub.Repository;
+
+namespace ProvaPub.Services
+{
+    public class CustomerService
+    {
+        TestDbContext _ctx;
+
+        public CustomerService(TestDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public CustomerList ListCustomers(int page)
+        {
+            return new CustomerList() { HasNext = false, TotalCount = 10, Customers = _ctx.Customers.ToList() };
+        }
+    }
+}
