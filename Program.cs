e95172c2ce1365b47d7caca7bc84a64d1c71eebd
igorsaulo <igorsaulo01@gmail.com>
2023-11-09 19:40:48 -0300

    confiure cors

--- Program.cs
+++ Program.cs
@@ -7,25 +7,38 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy("CorsPolicy",
+		builder => builder
+			.AllowAnyOrigin()
+			.AllowAnyMethod()
+			.AllowAnyHeader());
+});
 
 builder.Services.AddSingleton<RandomService>();
-builder.Services.AddDbContext<TestDbContext>(options =>
-	options.UseSqlite("Data Source=Test.db"));
+builder.Services.AddDbContext<TestDbContext>(options => options.UseSqlite("Data Source=Test.db"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+	app.UseCors("CorsPolicy");
 }
 
 app.UseHttpsRedirection();
 
+app.UseCors();
+    
 app.UseAuthorization();
+
 
 app.MapControllers();
 
